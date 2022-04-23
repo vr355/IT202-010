eee54fd3ef248cab6ef234e5133a373d21c4ec40
vr355 <vr355@njit.edu>
2022-04-23 14:06:46 +0500

    scripts added

---
+++ public_html/Project/sql/007_initial_setup_scripts.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+  Users(id, email, username, password)
+VALUES(-1, 'admin@gmail.com', 'admin', 'admin');
+INSERT INTO
+  Accounts(account_number, user_id, account_type, balance)
+VALUES
+  ('000000000000', '-1', 'world', '100000');
