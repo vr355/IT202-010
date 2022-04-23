fa1dbfc5414e27c64571ac6fbd427f1724a8c509
vr355 <vr355@njit.edu>
2022-04-24 03:53:26 +0500

    fixed transaction bug

--- public_html/Project/transaction.php
+++ public_html/Project/transaction.php
@@ -94,7 +94,7 @@ if (isset($_REQUEST["type"])) {
     </div>
     <div class="form-group col-md-3">
         <label for="change">Amount</label>
-        <input type="number" name="change" value="<?php se($_POST, '', 'change'); ?>"> />
+        <input type="number" name="change" value="<?php se($_POST, '', 'change'); ?>" />
     </div>
     <div class="form-group col-md-3">
         <label for="change">Memo</label>
