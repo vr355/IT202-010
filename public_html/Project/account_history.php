b77c4b6c9eacc977e12e9ef60b64b5a9913e0a90
vr355 <vr355@njit.edu>
2022-05-12 18:16:54 +0500

    added pagination

--- public_html/Project/account_history.php
+++ public_html/Project/account_history.php
@@ -6,6 +6,13 @@ $account = null;
 if (isset($_GET['id'])) {
     $db = getDB();
     $id = $_GET['id'];
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    $results_per_page = 10;
+    $page_first_result = ($page - 1) * $results_per_page;
+
+    //determine the total number of pages available  
+    $number_of_page = ceil(getTotalResults($db,  $id) / $results_per_page);
+
     try {
         $stmt = $db->prepare("SELECT *
          from Accounts 
@@ -14,13 +21,14 @@ if (isset($_GET['id'])) {
         if ($r) {
             $account = $stmt->fetch(PDO::FETCH_ASSOC);
             if ($account) {
-                $stmt = $db->prepare("SELECT a.account_number as 'src/dest'
+                $stmt = $db->prepare("SELECT b.account_number as 'source', a.account_number as dest
                 , transaction_type, memo, 
-                balance_change, expected_total, t.modified
+                balance_change, t.modified
                  from Transactions t
-                 join Accounts a on t.account_dest = a.id
-                 where account_src=:id or (account_dest=:id and account_src <> -1 and transaction_type <> 'transfer')
-                 order by modified desc limit 10");
+                 join Accounts a on t.account_src = a.id
+                 join Accounts b on t.account_dest = b.id
+                 where account_src=:id or account_dest=:id
+                 order by modified desc limit $page_first_result, $results_per_page");
                 $r = $stmt->execute(['id' => $id]);
                 if ($r) {
                     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -32,6 +40,23 @@ if (isset($_GET['id'])) {
     } catch (\Throwable $e) {
         flash("<pre>" . var_export($e, true) . "</pre>");
     }
+}
+
+function getTotalResults($db, $id)
+{
+    $query = "SELECT b.account_number as 'source', a.account_number as dest
+    , transaction_type, memo, 
+    balance_change, t.modified
+     from Transactions t
+     join Accounts a on t.account_src = a.id
+     join Accounts b on t.account_dest = b.id
+     where account_src=:id or account_dest=:id
+     order by modified desc";
+    $stmt =   $db->prepare($query);
+    $stmt->execute([
+        'id' => $id
+    ]);
+    return $stmt->rowCount();
 }
 
 ?>
@@ -67,7 +92,20 @@ if (isset($_GET['id'])) {
             <?php endforeach; ?>
         </table>
     <?php endif; ?>
+    <div>
 
+        <ul class="pagination  justify-content-center">
+            <?php if ($page >= 2) : ?>
+                <li class="page-item"><a class="page-link" href="<?php echo "account_history.php?id=$id&page=" . ($page - 1) ?>">Previous</a></li>
+            <?php endif ?>
+            <?php for ($p = 0; $p < $number_of_page; $p++) : ?>
+                <li class="page-item <?php echo $page == ($p + 1) ? 'active' : '' ?>"><a class="page-link" href="<?php echo "account_history.php?id=$id&page=" . ($p + 1) ?>"><?php echo ($p + 1) ?></a></li>
+            <?php endfor; ?>
+            <?php if ($page < $number_of_page) : ?>
+                <li class="page-item"><a class="page-link" href="<?php echo "account_history.php?id=$id&page=" . ($page + 1) ?>">Next</a></li>
+            <?php endif ?>
+        </ul>
+    </div>
     <?php
     require_once(__DIR__ . "/../../partials/flash.php");
     ?>
